60beb89fba010eab6eb9b4e6e82f28929e0bf6eb
Harphies <tafeez.tech@gmail.com>
2020-05-30 09:21:27 +0100

    new changes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ require("dotenv/config");
 const cors = require("cors");
 const app = express();
 const router = require("./router");
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 // Middlewares
 app.use(cors());
 app.use(bodyParser.json());
