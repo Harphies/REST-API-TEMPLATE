136384901182f8d92723035dcf26492f9338b4a0
Harphies <tafeez.tech@gmail.com>
2020-06-01 09:42:49 +0100

    fil import

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Posts = require("./model/User");
+const Posts = require("../model/User");
 
 router.get("/all", async (req, res) => {
   try {
