cd820ee50f95ad161de2e7c2096e58011a4051c2
Harphies <tafeez.tech@gmail.com>
2020-06-03 14:36:21 +0100

    crud

---
+++ Notes.md
@@ -0,0 +1,4 @@
+## essential Notes
+
+- We need to generate a different token with unique toen secret for every action and verify it when the user needs to access an action that depends on it.
+- deeper understanding of req.body and req.params
--- src/routes/postRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { authToken } = require("../middleware/verifyToken");
-const router = express.Router();
-const { getPosts } = require("../controllers/postController");
-
-router.get("/", authToken, getPosts);
-
-module.exports = router;
